b1bb6262caf42d23d939d5363892501edbc03b09
Zloy Rabadaber <zrabadaber@hotmail.com>
2016-01-19 23:59:37 +0300

    начало

---
+++ src/main/java/ru/zr/pluginTwo.java
@@ -0,0 +1,10 @@
+package ru.zr;
+
+/**
+ * Created by zraba on 19.01.2016.
+ */
+public class pluginTwo implements IPlugin {
+    public void execute() {
+        System.out.print("Work too\n");
+    }
+}
